b471198e8171831daeadee7a2a4710ef047d445b
rafaelTiAndiamo <rafael.ti@andiamo.com.br>
2025-10-24 09:15:16 -0300

    Corrige import case-sensitive do Sidebar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,32 +1,21 @@
 "use client";
 
-import { useState } from "react";
 import Image from "next/image";
-import { FileText, LogOut, UserCircle2 } from "lucide-react";
-import { useRouter } from "next/navigation";
+
+
 import { useUser } from "@/components/ui/UserContext";
 import { useAuthUser } from "@/hooks/useAuthUser";
-import { signOut } from "firebase/auth";
-import { auth } from "@/lib/firebaseConfig";
 
-const capitalizeFirstLetter = (str: string) =>
-  str ? str.charAt(0).toUpperCase() + str.slice(1).toLowerCase() : "";
+
+
 
 export default function Navbar() {
-  const [logoutError, setLogoutError] = useState<string | null>(null);
-  const router = useRouter();
+  
+
   const { email, setUser } = useUser();
   const { userData } = useAuthUser();
 
-  const handleLogout = async () => {
-    try {
-      await signOut(auth);
-      setUser("", "");
-      router.push("/login");
-    } catch (err) {
-      setLogoutError("Erro ao deslogar. Tente novamente.");
-    }
-  };
+  
 
   if (!email || !userData) return null;
 
